5ddfce46dbf2c4f5c431e13e3b0a63e9bc0fbcaf
lemon.b.citrom <lemon.b.citrom@protonmail.com>
2024-07-29 19:18:31 -0400

    Use MersenneTwister19937 for rng

--- src/utils/getRandom.ts
+++ src/utils/getRandom.ts
@@ -1,5 +1,16 @@
+/*
 export default function getRandom(maxValue: number, minValue: number = 1) {
   const min = Math.ceil(minValue);
   const max = Math.floor(maxValue);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+*/
+
+const { Random, MersenneTwister19937, nodeCrypto, browserCrypto } = require("random-js");
+const random = new Random(MersenneTwister19937.autoSeed());
+
+export default function getRandom(maxValue: number, minValue: number = 1) {
+  const min = Math.ceil(minValue);
+  const max = Math.floor(maxValue);
+  return random.integer(min,max);
 }
